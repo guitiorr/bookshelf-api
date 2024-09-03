67b9c73581282f8b69272e1b11b15b2fc8332694
guitiorr <officialglude12345@gmail.com>
2024-09-03 14:00:53 +0700

    update insert new book handler

--- src/handler/insertNewBookHandler.js
+++ src/handler/insertNewBookHandler.js
@@ -2,9 +2,28 @@ const { nanoid } = require("nanoid");
 const books = require("../object/books.js");
 
 const insertNewBookHandler = (request, h) => {
-  const { title, author, genre, language} = request.payload;
+  const { title, author, genre, language, pageCount, pageRead} = request.payload;
   const id = "BK-" + nanoid(3).toString();
   // const id = nanoid(3);
+
+  //Klo gak ada title
+  if (!title) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal menambahkan buku. Mohon isi nama buku.",
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (pageRead > pageCount) {
+    const response = h.response({
+      status: "fail",
+      message: "Jumlah halaman yang sudah dibaca lebih dari total halaman.",
+    });
+    response.code(400);
+    return response;
+  }
 
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
@@ -16,30 +35,32 @@ const insertNewBookHandler = (request, h) => {
     genre,
     language,
     insertedAt,
-    updatedAt
+    updatedAt,
+    pageCount
   };
 
   books.push(newBook);
 
-  const isSuccess = books.some((book) => book.id === id); // Updated from filter to some
+  const isSuccess = books.some((book) => book.id === id);
 
+  if(title )
 
   if (isSuccess) {
     const response = h.response({
       status: "success",
-      code: 201,
-      message: "Book added successfully",
+      message: "Buku berhasil ditambahkan",
       data: {
         bookId: id,
       },
     });
+    response.code(201);
     return response;
   } else {
     const response = h.response({
       status: "fail",
       message: "Fail to add new book",
-      code: 500,
     });
+    response.code(500);
     return response;
   }
 };
